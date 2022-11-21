e8fe7450d2fbf4d52051b9fdfe162cb21d45e8ca
johnkalb <jhonataaraujo665@gmail.com>
2022-11-21 16:17:07 -0300

    .

--- libera.js
+++ libera.js
@@ -1,25 +1,16 @@
-// var client = new HttpClient();
-// var request = new HttpRequestMessage();
-// request.RequestUri = new Uri("https://receitaws.com.br/v1/cnpj/31308464000193");
-// request.Method = HttpMethod.Get;
+// const options = {method: 'GET', headers: {'Content-Type': 'application/json'}};
 
-// request.Headers.Add("Accept", "*/*");
-// request.Headers.Add("User-Agent", "Thunder Client (https://www.thunderclient.com)");
-
-// var response = await client.SendAsync(request);
-// var result = await response.Content.ReadAsStringAsync();
-// Console.WriteLine(result);
+// fetch('https://receitaws.com.br/v1/cnpj/cnpj', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
 
 
 
-var client = new HttpClient();
-var request = new HttpRequestMessage();
-request.RequestUri = new Uri("https://api-publica.speedio.com.br/buscarcnpj?cnpj=31308464000193");
-request.Method = HttpMethod.Get;
-
-request.Headers.Add("Accept", "*/*");
-request.Headers.Add("User-Agent", "Thunder Client (https://www.thunderclient.com)");
-
-var response = await client.SendAsync(request);
-var result = await response.Content.ReadAsStringAsync();
-Console.WriteLine(result);
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#"+campo))
+        document.querySelector("#"+campo).value = result[campo]
+        // console.log(campo)
+    }
+}
