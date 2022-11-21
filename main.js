10664370af90e36b2d631860c1ce6f9f5957f449
johnkalb <jhonataaraujo665@gmail.com>
2022-11-21 15:18:18 -0300

    .

--- main.js
+++ main.js
@@ -3,24 +3,18 @@ const errorSpan = document.querySelector('#errorSpan')
 // const express = require('express')
 // const 
 
+// const URL = `https://api-publica.speedio.com.br/buscarcnpj?cnpj=${search}`;
 
-const showData = (result)=>{
-    for(const campo in result){
-        if(document.querySelector("#"+campo))
-        document.querySelector("#"+campo).value = result[campo]
-        // console.log(campo)
-    }
-}
 
 
 cnpj.addEventListener('blur', (e)=>{
     let search = cnpj.value.replace('.', '') //remove os caracteres do link
     
     let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
+    headers.append("Accept", "*/*");
+    headers.append("User-Agent", "Thunder Client (https://www.thunderclient.com)");
     // headers.append('Authorization', 'Basic ' + base64.encode(username + ":" +  password));
-    headers.append('Origin','http://localhost:5500');
+    headers.append('Origin','http://127.0.0.1:5500');
 
     const options = {
         method: 'GET',
@@ -29,9 +23,32 @@ cnpj.addEventListener('blur', (e)=>{
         headers: headers
     }
 
+
+    
+
+
+    const showData = (result)=>{
+        for(const campo in result){
+            if(document.querySelector("#"+campo))
+            document.querySelector("#"+campo).value = result[campo]
+            console.log(campo, "II")
+        }
+    }
   fetch(`https://api-publica.speedio.com.br/buscarcnpj?cnpj=${search}`, options)
-    .then(response=>{response.json()
-        .then(data=>showData(data), errorSpan.innerHTML=``)
+  .then((response)=>{response.json()
+    .then(data=>showData(data), errorSpan.innerHTML='')
+     .catch((e=> console.log('error')))
     })
     .catch(e=> console.log=``)
 })
+// var client = new HttpClient();
+// var request = new HttpRequestMessage();
+// request.RequestUri = new Uri("https://api-publica.speedio.com.br/buscarcnpj?cnpj=31308464000193");
+// request.Method = HttpMethod.Get;
+
+// request.Headers.Add("Accept", "*/*");
+// request.Headers.Add("User-Agent", "Thunder Client (https://www.thunderclient.com)");
+
+// var response = await client.SendAsync(request);
+// var result = await response.Content.ReadAsStringAsync();
+// Console.WriteLine(result);
