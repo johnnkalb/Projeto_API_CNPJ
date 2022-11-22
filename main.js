f7af49857dc7a1cc4588559a96b45644e9d907a3
johnkalb <jhonataaraujo665@gmail.com>
2022-11-22 14:22:13 -0300

    .

--- main.js
+++ main.js
@@ -6,12 +6,35 @@ const erro = document.querySelector('.erro')
 // const express = require('express')
 // const 
 
+// export interface Main {
+//     "NOME FANTASIA":            string;
+//     "RAZAO SOCIAL":             string;
+//     CNPJ:                       string;
+//     STATUS:                     string;
+//     "CNAE PRINCIPAL DESCRICAO": string;
+//     "CNAE PRINCIPAL CODIGO":    string;
+//     CEP:                        string;
+//     "DATA ABERTURA":            string;
+//     DDD:                        string;
+//     TELEFONE:                   string;
+//     EMAIL:                      string;
+//     "TIPO LOGRADOURO":          string;
+//     LOGRADOURO:                 string;
+//     NUMERO:                     string;
+//     COMPLEMENTO:                string;
+//     BAIRRO:                     string;
+//     MUNICIPIO:                  string;
+//     UF:                         string;
+// }
+
+
+// 32840757000180 CNPJ QUERY PARA SEARCH
 // const URL = `https://api-publica.speedio.com.br/buscarcnpj?cnpj=${search}`;
 
 
 
 cnpj.addEventListener('blur', (e)=>{
-    let search = cnpj.value.replace('.', '') //remove os caracteres do link
+    let search = cnpj.value.replace('.', '' & '/', '' & '-', '') //remove os caracteres do link
  
     let headers = new Headers();
     headers.append("Accept", "*/*");
@@ -27,6 +50,7 @@ cnpj.addEventListener('blur', (e)=>{
 
     const showData = (result)=>{
         for(const campo in result){
+            // let resposta = result.value.replace(' ', '')
             if(document.querySelector('#'+campo))
             // document.querySelector("#"+campo).value = result[campo]
             document.querySelector('#'+campo).value = result[campo]
@@ -44,7 +68,7 @@ cnpj.addEventListener('blur', (e)=>{
     //   .then(response => console.table(response))
       .then(response => showData(response))
       
-      .catch(err => console.error(err)), form.reset(), erro.nodeValue=`${response.error}`;
+      .catch(err => console.error(err)), form.reset();
 })
 
 // function (showData){
