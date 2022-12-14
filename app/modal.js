1dde94b17ad30f7f0d336d3116a3e12d10589229
johnkalb <jhonataaraujo665@gmail.com>
2022-12-14 15:45:35 -0300

    .

--- app/modal.js
+++ app/modal.js
@@ -15,26 +15,13 @@ const switchModal = () => {
   
   const btn = document.querySelector('.modalBtn')
   btn.addEventListener('click', switchModal);
+
+  const 
   
   window.onclick = function(event) {
       const modal = document.querySelector('.modal')
     if (event.target == modal) {
       switchModal();
       form.reset();
-    }
-  }
-
-  fechaModal = () =>{
-    const modal = document.querySelector('.modal')
-    const actualStyle = modal.style.display
-
-    if(actualStyle == 'block') {
-      modal.style.display = 'none'
-      
-      // cadastrado.style.display = 'none';
-      erro.style.display = 'none';
-    }
-    else {
-      modal.style.display = 'block'
     }
   }
