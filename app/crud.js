0bec226c2617fe5d3afeaacafe9b92a792fd61de
johnkalb <jhonataaraujo665@gmail.com>
2022-12-15 15:28:47 -0300

    .

--- app/crud.js
+++ app/crud.js
@@ -66,52 +66,46 @@ const existe = itens.find(elemento => elemento.CNPJ === CNPJ.value)
 
  const saveConsulta = (e) =>{
     if(isValidConsulta()){
-        if(existe){
-            consulta.id = existe.id 
-            console.log('já Existe')
-        }else{
-            const consulta = {
-                // A_PESQUISA: document.querySelector('#NOMEDEPESQUISA').value,
-                CNPJ: document.querySelector('#CNPJ').value,
-                NOME_FANTASIA: document.querySelector('#NOMEFANTASIA').value,
-                RAZAO_SOCIAL: document.querySelector('#RAZAOSOCIAL').value,
-                STATUS: document.querySelector('#SITUACAO').value,
-                CNAE_PRINCIPAL_DESCRICAO: document.querySelector('#CNAEDESC').value,
-    
-                    CNAE_PRINCIPAL_CODIGO: document.querySelector('#CNAECOD').value,
-                    CEP: document.querySelector('#CEP').value,
-                    DATA_ABERTURA: document.querySelector('#ABERTURA').value,
-                    DDD: document.querySelector('#DDD').value,
-    
-                        TELEFONE: document.querySelector('#TELEFONE').value,
-                        EMAIL:document.querySelector('#EMAIL').value,
-                        TIPO_LOGRADOURO: document.querySelector('#TIPOLOGRADOURO').value,
-                        LOGRADOURO: document.querySelector('#LOGRADOURO').value,
-                        NUMERO: document.querySelector('#NUMERO').value,
-    
-                            COMPLEMENTO: document.querySelector('#COMPLEMENTO').value,
-                            BAIRRO: document.querySelector('#BAIRRO').value,
-                            MUNICIPIO: document.querySelector('#MUNICIPIO').value,
-                            UF: document.querySelector('#UF').value,
-            }
-            
-            
-            const index = document.getElementById('CNPJ').dataset.index
-            e.preventDefault()
-            fechaModal()
-        if (index=='new'){
-            
-            createConsulta(consulta)
-            updadeTable()
-            abreModalExibe()
-            // location.reload()
-        }else{
-            updadeConsulta(index, consulta)
-            updadeTable()
-            abreModalExibe()
+        const consulta = {
+            // A_PESQUISA: document.querySelector('#NOMEDEPESQUISA').value,
+            CNPJ: document.querySelector('#CNPJ').value,
+            NOME_FANTASIA: document.querySelector('#NOMEFANTASIA').value,
+            RAZAO_SOCIAL: document.querySelector('#RAZAOSOCIAL').value,
+            STATUS: document.querySelector('#SITUACAO').value,
+            CNAE_PRINCIPAL_DESCRICAO: document.querySelector('#CNAEDESC').value,
+
+                CNAE_PRINCIPAL_CODIGO: document.querySelector('#CNAECOD').value,
+                CEP: document.querySelector('#CEP').value,
+                DATA_ABERTURA: document.querySelector('#ABERTURA').value,
+                DDD: document.querySelector('#DDD').value,
+
+                    TELEFONE: document.querySelector('#TELEFONE').value,
+                    EMAIL:document.querySelector('#EMAIL').value,
+                    TIPO_LOGRADOURO: document.querySelector('#TIPOLOGRADOURO').value,
+                    LOGRADOURO: document.querySelector('#LOGRADOURO').value,
+                    NUMERO: document.querySelector('#NUMERO').value,
+
+                        COMPLEMENTO: document.querySelector('#COMPLEMENTO').value,
+                        BAIRRO: document.querySelector('#BAIRRO').value,
+                        MUNICIPIO: document.querySelector('#MUNICIPIO').value,
+                        UF: document.querySelector('#UF').value,
         }
-        } }
+        const index = document.getElementById('CNPJ').dataset.index
+        e.preventDefault()
+        fechaModal()
+    if (index=='new'){
+        createConsulta(consulta)
+        updadeTable()
+        abreModalExibe()
+        // location.reload()
+    }else{
+        updadeConsulta(index, consulta)
+        updadeTable()
+        abreModalExibe()
     }
+    }
+}
+
     // }
 
     const createRow = (consulta, index) =>{
